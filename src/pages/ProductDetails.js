b7cd4fb725fa33f95bf8737f7950f841a533d6bb
pradip1232 <755201pradip@gmail.com>
2025-05-12 17:49:06 +0530

    animations

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -38,24 +38,28 @@ const ProductDetails = () => {
             <section className='product-details-section'>
                 <Container>
                     <div className='product-details-content'>
-                        <Row>
-                            <Col md={5} lg={5} xs={12} sm={11} className='text-end'>
-                                <img src={ace} className='img-fluid product-details-img' />
+                        <Row className="align-items-center">
+                            <Col md={5} lg={5} xs={12} sm={11} className='text-end mb-4 mb-md-0'>
+                                <img src={ace} alt="Acepron Injection" className='img-fluid product-details-img' />
                             </Col>
                             <Col md={6} lg={6} xs={12} sm={11}>
                                 <h4 className='product-name'>Acepron</h4>
                                 <p className='product-description'>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                                    <br /> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                    <br />
+                                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                 </p>
                                 <p className='product-price'>$ 10.99</p>
                                 {/* <p className='product-quantity'></p> */}
-                                <h6 className='products-tags'>Target Species: Cats, Dogs</h6>
-                                <button className='btn btn-primary'>Technical Enquiry</button>
-                                <button className='btn btn-secondary'>Pack Insert</button>
+                                <h6 className='products-tags'>Target Species: Cats, Dogs</h6>
+                                <div className="d-flex flex-wrap gap-2 mt-3">
+                                    <button className='btn btn-primary'>Technical Enquiry</button>
+                                    <button className='btn btn-secondary'>Pack Insert</button>
+                                </div>
                             </Col>
                         </Row>
                     </div>
+
                 </Container>
 
             </section>
